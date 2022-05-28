e46e31ca8c581a11ab496f7c219c72eeb40c2021
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-05-28 19:24:13 +0300

    up

---
+++ Advanced C-Sharp/Stacks And Queues_EXERCISE/10.Crossroads/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace _10.Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            Queue<string> carQueue = new Queue<string>();
+
+            string command = Console.ReadLine();
+            int carCounter = 0;
+
+            while (command != "END")
+            {
+                if (command == "green")
+                {
+                    string currentCar = carQueue.Dequeue();
+                    int timeToPass = greenLight + freeWindow;
+
+                    while (timeToPass > 0)
+                    {
+                        if (currentCar.Length > timeToPass)
+                        {
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{currentCar} was hit at {currentCar.Substring(timeToPass,1)}");
+                            goto endProgram;
+                        }
+                        else
+                        {
+                            carCounter++;
+                            timeToPass -= currentCar.Length;
+                            if (timeToPass <= freeWindow)
+                            {
+                                goto newCommand;
+                            }
+                            else
+                            {
+                                if (carQueue.Count > 0)
+                                {
+                                currentCar = carQueue.Dequeue();
+                                }
+                                else
+                                {
+                                    goto newCommand;
+                                }
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    carQueue.Enqueue(command);
+                }
+                newCommand:;
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{carCounter} total cars passed the crossroads.");
+        endProgram:;
+        }
+    }
+}
