6ea5d6333292db5442c433095d0b80637eb92550
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-06 16:57:36 +0300

    up

---
+++ Advanced C-Sharp/Multidimensional Arrays_LAB/3.PrimaryDiagonal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] table = new int[n, n];
+
+            for (int row = 0; row < table.GetLength(0); row++)
+            {
+                int[] colNumbers = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < table.GetLength(1); col++)
+                {
+                    table[row, col] = colNumbers[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int row = 0; row < table.GetLength(0); row++)
+            {
+                sum += table[row, row];
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
