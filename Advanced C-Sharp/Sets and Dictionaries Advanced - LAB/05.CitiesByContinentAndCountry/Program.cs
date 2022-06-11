39d3f552927e364596c1434a77649729c3921f7e
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-11 18:35:43 +0300

    up

--- Advanced C-Sharp/Sets and Dictionaries Advanced - LAB/05.CitiesByContinentAndCountry/Program.cs
+++ Advanced C-Sharp/Sets and Dictionaries Advanced - LAB/05.CitiesByContinentAndCountry/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05.CitiesByContinentAndCountry
 {
@@ -6,7 +8,38 @@ namespace _05.CitiesByContinentAndCountry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (continents.ContainsKey(continent) == false)
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (continents[continent].ContainsKey(country) == false)
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+                continents[continent][country].Add(city);
+            }
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var county in continent.Value)
+                {
+                    Console.WriteLine($"  {county.Key} -> {string.Join(", ", county.Value)}");
+                }
+            }
         }
     }
 }
