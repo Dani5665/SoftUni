810bc10ba593ca7f39b1661212ed2cff27559f51
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-12 12:53:14 +0300

    up

---
+++ Advanced C-Sharp/Sets and Dictionaries Advanced - EXERCISE/02. SetsOfElements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+            int[] lenghts = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < lenghts[0]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set1.Add(number);
+            }
+            for (int i = 0; i < lenghts[1]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set2.Add(number);
+            }
+
+            HashSet<int> repeatedNumbers = new HashSet<int>();
+
+            foreach (var number in set1)
+            {
+                if (set2.Contains(number) && repeatedNumbers.Contains(number) == false)
+                {
+                    repeatedNumbers.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", repeatedNumbers));
+        }
+    }
+}
