20b0793001b4d84bf678374fe14b022c915c53d8
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-12 11:56:26 +0300

    up

---
+++ Advanced C-Sharp/Sets and Dictionaries Advanced - LAB/08.SoftUniParty/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            HashSet<string> normalGuestList = new HashSet<string>();
+            HashSet<string> vipGuestList = new HashSet<string>();
+
+            while (command != "PARTY")
+            {
+                bool result = char.IsDigit(command, 0);
+                if (command.Length == 8 && result)
+                {
+                    vipGuestList.Add(command);
+                }
+                else if (command.Length == 8 && result == false)
+                {
+                    normalGuestList.Add(command);
+                }
+                command = Console.ReadLine();
+            }
+
+            while (command != "END")
+            {
+                if (vipGuestList.Contains(command))
+                {
+                    vipGuestList.Remove(command);
+                }
+                else if (normalGuestList.Contains(command))
+                {
+                    normalGuestList.Remove(command);
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(vipGuestList.Count + normalGuestList.Count);
+
+            foreach (var guest in vipGuestList)
+            {
+                Console.WriteLine(guest);
+            }
+            foreach (var guest in normalGuestList)
+            {
+                Console.WriteLine(guest);
+            }
+
+
+        }
+    }
+}
