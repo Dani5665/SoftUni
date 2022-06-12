cf2c8a5bd697f8b2b566fd161ae088fef6c5bc7f
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-12 12:40:38 +0300

    up

---
+++ Advanced C-Sharp/Sets and Dictionaries Advanced - EXERCISE/01.UniqueUsernames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string currName = Console.ReadLine();
+                if (names.Contains(currName) == false)
+                {
+                    names.Add(currName);
+                }
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
