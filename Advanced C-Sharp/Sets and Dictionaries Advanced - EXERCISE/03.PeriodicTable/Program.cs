35cec6292ac4ff99c2ddbcb289601f8d8cfcc30b
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-12 14:19:37 +0300

    up

---
+++ Advanced C-Sharp/Sets and Dictionaries Advanced - EXERCISE/03.PeriodicTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currElements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                for (int j = 0; j < currElements.Length; j++)
+                {
+                    if (elements.Contains(currElements[j]) == false)
+                    {
+                        elements.Add(currElements[j]);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
