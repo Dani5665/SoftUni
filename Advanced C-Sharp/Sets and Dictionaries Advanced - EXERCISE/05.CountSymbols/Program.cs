3effa17c91ff1b05f11927463575b94b35a37ad0
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-12 14:35:36 +0300

    up

---
+++ Advanced C-Sharp/Sets and Dictionaries Advanced - EXERCISE/05.CountSymbols/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> table = new SortedDictionary<char, int>();
+            string input = Console.ReadLine();
+
+            foreach (char ch in input)
+            {
+                if (table.ContainsKey(ch) == false)
+                {
+                    table.Add(ch, 0);
+                }
+                table[ch]++;
+            }
+
+            foreach (var ch in table)
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}
