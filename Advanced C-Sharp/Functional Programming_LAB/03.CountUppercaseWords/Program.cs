36a876117d725e5f3709a56cf342c579fbe6999d
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-14 22:46:55 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_LAB/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => char.IsUpper(x[0]));
+
+            foreach (var item in input)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
