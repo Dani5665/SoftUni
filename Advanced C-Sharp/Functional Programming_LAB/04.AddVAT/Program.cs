35b22109bd1bef99f862f63ba31b8bc512b12da4
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-14 22:50:55 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_LAB/04.AddVAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x *= 1.2);
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
