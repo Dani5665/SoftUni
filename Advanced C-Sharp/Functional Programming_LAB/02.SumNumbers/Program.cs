26b2b88133ce02456514aa7880f938812345c940
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-14 22:43:58 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_LAB/02.SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                ;
+            var count = list.Count();
+            var average = list.Sum();
+
+            Console.WriteLine(count);
+            Console.WriteLine(average);
+        }
+    }
+}
