b90ba04c0e1d88f6eb838db739f832339a6ca057
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-14 22:39:56 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_LAB/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .Where(x => x % 2 == 0);
+
+            Console.WriteLine(string.Join(", ", numbers));
+                
+        }
+    }
+}
