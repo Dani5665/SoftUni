23605a7438b1bc73e803d24e429cee117d00bfcf
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-15 16:10:34 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_EXERCISE/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> isDivisible = (x, y) => x % y != 0;
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int num = int.Parse(Console.ReadLine());
+
+            numbers = numbers.Where(x => isDivisible(x, num))
+                .Reverse()
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
