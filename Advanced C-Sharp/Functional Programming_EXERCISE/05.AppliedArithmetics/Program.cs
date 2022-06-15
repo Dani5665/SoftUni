c5ad35982259baadcec4d71d324ea632f43d807a
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-15 15:48:23 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_EXERCISE/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace _05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int[]> operation = null;
+            Action<int[]> print = x => Console.WriteLine(string.Join(" ", x));
+
+            string command = Console.ReadLine();
+
+            while (command!="end")
+            {
+                if (command=="add")
+                {
+                    operation = numbers => 
+                    {
+                        for (int i = 0; i < list.Length; i++)
+                        {
+                            list[i] += 1;
+                        }
+                        return numbers;
+                    };
+                    operation(list);
+                }
+                else if (command=="multiply")
+                {
+                    operation = x =>
+                    {
+                        for (int i = 0; i < list.Length; i++)
+                        {
+                            list[i] *= 2;
+                        }
+                        return list;
+                    };
+                    operation(list);
+                }
+                else if (command=="subtract")
+                {
+                    operation = x =>
+                    {
+                        for (int i = 0; i < list.Length; i++)
+                        {
+                            list[i] -= 1;
+                        }
+                        return list;
+                    };
+                    operation(list);
+                }
+                else if (command=="print")
+                {
+                    print(list);
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
