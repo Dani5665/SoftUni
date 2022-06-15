74b833f5483e57b837eb1d06a004117836a4aa85
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-15 14:38:26 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_EXERCISE/03.CustomMinFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double[], double> smallestNumber = x => x.Min();
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Console.WriteLine(smallestNumber(numbers));
+        }
+    }
+}
