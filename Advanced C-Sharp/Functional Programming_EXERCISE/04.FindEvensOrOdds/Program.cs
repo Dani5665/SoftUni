e83f3b80548d737a08f6ef5e69bfbf32808fae96
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-15 15:00:38 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_EXERCISE/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var numbers = Enumerable.Range(input[0], input[1]-input[0]+1);
+
+            string command = Console.ReadLine();
+
+            if (command == "even")
+            {
+                Console.WriteLine(string.Join(" ",numbers.Where(x=>x%2==0)));
+            }
+            else if (command == "odd")
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 != 0)));
+            }
+        }
+    }
+}
