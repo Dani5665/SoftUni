22f127187b43997f67baa79ac15fcafe1b7a1dee
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-15 14:25:32 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_EXERCISE/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> consoleWrite = x => Console.WriteLine($"Sir {x}");
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var name in input)
+            {
+                consoleWrite(name);
+            }
+        }
+    }
+}
