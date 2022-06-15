200f0d987c98702af66b582f77c6aa9d946ea7c2
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-15 16:26:25 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_EXERCISE/07.PredicateForNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            Func<string, bool> nameCheck = x => x.Length <= lenght;
+
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => nameCheck(x))
+                .ToArray();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+                
+        }
+    }
+}
