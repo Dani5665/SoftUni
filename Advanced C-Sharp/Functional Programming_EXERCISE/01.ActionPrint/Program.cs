945f3f0aa30ae696bcf2670cd7fb6951bdfc2831
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-06-15 14:19:41 +0300

    up

---
+++ Advanced C-Sharp/Functional Programming_EXERCISE/01.ActionPrint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> consoleWrite = x => Console.WriteLine(x);
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var name in input)
+            {
+                consoleWrite(name);
+            }
+                
+        }
+    }
+}
