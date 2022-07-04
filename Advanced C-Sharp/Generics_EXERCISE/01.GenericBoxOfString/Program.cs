04ccdf801c469b97062797d5dd6bb9bb350b24e4
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-07-04 10:52:50 +0300

    up

---
+++ Advanced C-Sharp/Generics_EXERCISE/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                Console.WriteLine($"{box.PrintBoxType()}: {box.PrintBoxValue()}");
+            }
+        }
+    }
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public string PrintBoxType()
+        {
+            return item.GetType().ToString();
+        }
+        public string PrintBoxValue()
+        {
+            return item.ToString();
+        }
+    }
+}
