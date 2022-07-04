83c54adf8765278f0f1dffff3ab5c7a05494f295
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-07-04 14:21:51 +0300

    up

--- Advanced C-Sharp/Generics_EXERCISE/07.Tuple/Program.cs
+++ Advanced C-Sharp/Generics_EXERCISE/07.Tuple/Program.cs
@@ -6,7 +6,31 @@ namespace _07.Tuple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputNameTown = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = inputNameTown[0] + " " + inputNameTown[1];
+            string town = inputNameTown[2];
+            if (inputNameTown.Length == 4)
+            {
+                town = inputNameTown[2] + " " + inputNameTown[3];
+            }                      
+            MyTuple<string, string> nameTown =
+                new MyTuple<string, string>(name, town);
+
+            string[] inputNameLiters = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name2 = inputNameLiters[0];
+            int liters = int.Parse(inputNameLiters[1]);
+            MyTuple<string, int> nameLiters =
+                new MyTuple<string, int>(name2, liters);
+
+            string[] inputNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int num1 = int.Parse(inputNumbers[0]);
+            double num2 = double.Parse(inputNumbers[1]);
+            MyTuple<int, double> numbers =
+                new MyTuple<int, double>(num1, num2);
+
+            Console.WriteLine(nameTown.GetItems());
+            Console.WriteLine(nameLiters.GetItems());
+            Console.WriteLine(numbers.GetItems());
         }
     }
 }
---
+++ Advanced C-Sharp/Generics_EXERCISE/07.Tuple/Tuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class MyTuple<T, K>
+    {
+        private T item1;
+        private K item2;
+
+        public T Item1 { get => item1; set => item1 = value; }
+        public K Item2 { get => item2; set => item2 = value; }
+
+        public MyTuple(T item1, K item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public string GetItems()
+        {
+            return $"{item1} -> {item2}";
+        }
+    }
+}
