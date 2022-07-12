33aa464f8932e31819c8325aa6671340c9e7eef5
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-07-12 21:15:31 +0300

    up

---
+++ OOP C-Sharp/Inheritance_LAB/CustomStack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "1", "2", "3", "4", "5", "6", "7" };
+            var stack = new StackOfStrings();
+            stack.AddRange(list);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
---
+++ OOP C-Sharp/Inheritance_LAB/CustomStack/StackOfStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void AddRange(IEnumerable<string> stack)
+        {
+            foreach (var item in stack)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
