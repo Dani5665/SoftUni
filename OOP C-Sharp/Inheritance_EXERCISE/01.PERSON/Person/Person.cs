fbf41ba8415a96eed1902a5fed57d66948547449
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-07-13 14:07:46 +0300

    up

---
+++ OOP C-Sharp/Inheritance_EXERCISE/01.PERSON/Person/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Person
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        
+
+        public override string ToString()
+        {
+            string overritedToStringMethod = $"Name: {this.Name}, Age: {this.Age}";
+            return overritedToStringMethod;
+        }
+    }
+}
---
+++ OOP C-Sharp/Inheritance_EXERCISE/01.PERSON/Person/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person human;
+            if (age < 0)
+            {
+                return;
+            }
+            if (age <= 15)
+            {
+                human = new Child(name, age);
+            }
+            else
+            {
+                human = new Person(name, age);
+            }
+
+            Console.WriteLine(human);
+        }
+    }
+}
