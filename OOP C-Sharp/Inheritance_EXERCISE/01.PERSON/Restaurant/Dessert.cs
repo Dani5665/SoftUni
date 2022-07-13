92410a17238fd4c8a53b9be1835919ec63389899
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-07-13 18:48:52 +0300

    up

---
+++ OOP C-Sharp/Inheritance_EXERCISE/01.PERSON/Restaurant/Dessert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Dessert : Food
+    {
+        public Dessert(string name, decimal price, double grams, double callories) 
+            : base(name, price, grams)
+        {
+            Callories = callories;
+        }
+        public double Callories { get; set; }
+    }
+}
