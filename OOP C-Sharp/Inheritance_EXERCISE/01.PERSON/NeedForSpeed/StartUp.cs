06c97759dcb37c5a3a4ec0c6e907015a7a5140de
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-07-13 17:41:59 +0300

    up

--- OOP C-Sharp/Inheritance_EXERCISE/01.PERSON/NeedForSpeed/StartUp.cs
+++ OOP C-Sharp/Inheritance_EXERCISE/01.PERSON/NeedForSpeed/StartUp.cs
@@ -4,7 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            
+            var sportCar = new SportCar(10, 10);
+            sportCar.Drive(1);
+            System.Console.WriteLine();
         }
     }
 }
