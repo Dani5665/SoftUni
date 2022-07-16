f2fb7f3c5d32d684b39bb95114422db3ba3a9c47
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2022-07-16 21:35:20 +0300

    up

--- OOP C-Sharp/Encapsulation_LAB/PersonsInfo/Program.cs
+++ OOP C-Sharp/Encapsulation_LAB/PersonsInfo/Program.cs
@@ -8,22 +8,11 @@ namespace PersonsInfo
     {
         static void Main(string[] args)
         {
-            var lines = int.Parse(Console.ReadLine());
-            var persons = new List<Person>();
-            for (int i = 0; i < lines; i++)
-            {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0],
-                                        cmdArgs[1],
-                                        int.Parse(cmdArgs[2]),
-                                        decimal.Parse(cmdArgs[3]));
-
-                persons.Add(person);
-            }
-            var parcentage = decimal.Parse(Console.ReadLine());
-            persons.ForEach(p => p.IncreaseSalary(parcentage));
-            persons.ForEach(p => Console.WriteLine(p.ToString()));
-
+            Team team = new Team("SU");
+            Person person = new Person("aaaa", "bbbb", 12, 1000);
+            team.AddPlayer(person);
+            Console.WriteLine(team.FirstTeam.Count);
+            Console.WriteLine(team.ReserveTeam.Count);
         }
     }
 }
